1bf3745f4b2178bd250063dd57fbc695409b96d1
miceuz <mic@wemakethings.net>
2016-05-25 12:51:55 +0300

    Add all the functionality at one go: commands to turn on/off the laser, report status, calibration value

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include "SPI.h"
 #include "timer.h"
 #include <avr/interrupt.h>
+#include <avr/eeprom.h>
 #include "RFM69.h"
 
 inline static void digipotDeselect() {
@@ -45,15 +46,41 @@ inline static void laserInit() {
 
 
 inline static void detectorInit() {
-	//laser detector as input
+	digipotInit();
+
+	//comparator output pin as input
 	DDRB &= ~_BV(PB1);
+
+	MCUCR |= _BV(ISC01); //falling edge on INT0
+	GIMSK |= _BV(INT0);
+	digipotSet(0);
 }
 
-inline static void adcInit() {
-	ADCSRA = _BV(ADEN) | _BV(ADPS0) | _BV(ADPS1) | _BV(ADPS2);
-	DIDR1 = _BV(ADC11D);
-	ADMUXA = 11;
-	ADMUXB = _BV(REFS1); //internal 2.2 reference
+volatile uint8_t laserTripped = 0;
+
+inline static void detectorArm() {
+	laserTripped = 0;
+}
+
+uint8_t calibration = 0;
+
+inline static void detectorCalibrate() {
+
+	for(uint8_t b = 0; b < 255; b++) {
+		digipotSet(b);
+		delay(1);
+
+		if(laserTripped) {
+			if(b <= 255 - 10) {
+				calibration = b + 10;
+			} else {
+				calibration = b;
+			}
+			digipotSet(calibration);
+			break;
+		}
+	}
+	detectorArm();
 }
 
 inline static uint16_t getBatteryVoltage() {
@@ -64,6 +91,18 @@ inline static uint16_t getBatteryVoltage() {
 	return ADC;
 }
 
+inline static void adcInit() {
+	ADCSRA = _BV(ADEN) | _BV(ADPS0) | _BV(ADPS1) | _BV(ADPS2);
+	DIDR1 = _BV(ADC11D);
+	ADMUXA = 11;
+	ADMUXB = _BV(REFS1); //internal 2.2 reference
+	delay(10);
+	getBatteryVoltage();
+	delay(10);
+	getBatteryVoltage();
+}
+
+
 inline static void ledOn() {
 	PORTB |= _BV(PB2);
 }
@@ -72,20 +111,114 @@ inline static void ledOff() {
 	PORTB &= ~_BV(PB2);
 }
 
-//RFM69 radio;
-char *buff = "winrar 42";
+inline static uint8_t isBatteryLow() {
+	return getBatteryVoltage() < 698;
+}
+
+inline static void ledRapidBlink(uint8_t times) {
+	for(int i = 0; i < times; i++) {
+		ledOn();
+		delay(200);
+		ledOff();
+		delay(200);
+	}
+}
+
+RFM69 radio;
+
+static union {
+	char buff[10];
+	struct {
+		uint8_t nodeId;
+		uint8_t messageId;
+		uint16_t batteryVoltage;
+		uint8_t calibration;
+		uint8_t laserTripped;
+	} s;
+} packet;
+
+#define MESSAGE_STATUS 0
+#define GATEWAY_ADDRESS 254
+
+#define CMD_LASER_ON 1
+#define CMD_LASER_OFF 2
+#define CMD_CALIBRATE 3
+#define CMD_GET_STATUS 4
+
+void sendStatusPacket() {
+	packet.s.messageId = MESSAGE_STATUS;
+	packet.s.batteryVoltage = getBatteryVoltage();
+	packet.s.calibration = calibration;
+	packet.s.laserTripped = laserTripped;
+	radio.send(GATEWAY_ADDRESS, packet.buff, 9);
+}
+
+#define I2C_ADDRESS_EEPROM_LOCATION (uint8_t*)0x00
+
+inline void waitAndSendStatus() {
+	for(uint8_t i = 0; i < packet.s.nodeId; i++) {
+		delay(10);
+	}
+	sendStatusPacket();
+}
 
 int main() {
 	sei();
-	laserInit();
 	timerInit();
+
 	ledInit();
+	ledOn();
+	delay(100);
+	ledOff();
+
+	packet.s.nodeId = eeprom_read_byte(I2C_ADDRESS_EEPROM_LOCATION);
+
+	SPI.begin();
+	laserInit();
 	adcInit();
+
+	detectorInit();
+	detectorCalibrate();
+
+	uint8_t radioInitialised = radio.initialize(RF69_868MHZ, packet.s.nodeId, 100);
+	radio.encrypt(0);
+	if(radioInitialised) {
+		ledOn();
+		delay(1500);
+		sendStatusPacket();
+		ledOff();
+	} else {
+		ledRapidBlink(2);
+	}
+	
+	laserOn();
+
 	while(1) {
-		if(getBatteryVoltage() < 698) {
-			ledOff();
-		} else {
+		if(laserTripped) {
 			ledOn();
+			sendStatusPacket();
+			delay(10);
+			detectorArm();
+		} else {
+			ledOff();
+		}
+
+		if(radio.receiveDone()) {
+			switch((char)radio.DATA[0]) {
+				case CMD_LASER_ON: laserOn(); break;
+				case CMD_LASER_OFF: laserOff(); break;
+				case CMD_CALIBRATE: detectorCalibrate(); break;
+				case CMD_GET_STATUS: waitAndSendStatus(); break;
+			}
+		}
+
+		if(isBatteryLow()) {
+			laserOff();
+			radio.sleep();
+			while(1) {
+				ledRapidBlink(10);
+				delay(2000);
+			}
 		}
 	}
 }
@@ -133,3 +266,7 @@ int main2() {
 	return 0;
 }
 */
+
+ISR(INT0_vect){
+	laserTripped = 1;
+}
